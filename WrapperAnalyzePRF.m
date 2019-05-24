5a78673833743e456a6a5b0a5d848e676e039590
ozenc_taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2019-05-24 14:26:19 -0400

    Aded the Wrapper function

---
+++ WrapperAnalyzePRF.m
@@ -0,0 +1,107 @@
+function p = WrapperAnalyzePRF(stimFileName,dataFileName,tr,outpath,varargin)
+%% Parse vargin for options passed here
+
+p = inputParser; p.KeepUnmatched = true;
+
+% Required  
+p.addRequired('stimFileName',@isstr);
+p.addRequired('dataFileName',@isstr);
+p.addRequired('tr', @isstr);
+p.addRequired('outpath', @isstr);
+
+% Optional parameters
+p.addParameter('wantglmdenoise','0',@isstr);
+p.addParameter('hrf',[],@isstr);   %MAT FILE, COLUMN VECTOR
+p.addParameter('maxpolydeg',[],@isstr);
+p.addParameter('seedmode','[0 1 2]',@isstr);
+p.addParameter('xvalmode','0',@isstr);
+p.addParameter('numperjob',[],@isstr);
+p.addParameter('maxiter','500',@isstr);
+p.addParameter('display','iter',@isstr);
+p.addParameter('typicalgain','10',@isstr);
+p.addParameter('maskFileName',[], @isstr);
+
+% parse
+p.parse(stimFileName, dataFileName, tr, outpath, varargin{:})
+
+% Load the stimulus and data files
+load(stimFileName,'stimulus');
+
+%%nifti to 2d
+rawData = niftiread(p.Results.dataFileName);   % Load 4D data
+data = reshape(rawData, [size(rawData,1)*size(rawData,2)*size(rawData,3), size(rawData,4)]); % Convert 4D to 2D
+%data = data(:,1:300);
+data = im2single(data);   % convert data to single and scale to 0-1 range
+
+% massage cell inputs
+if ~iscell(stimulus)
+  stimulus = {stimulus};
+end
+if ~iscell(data)
+  data = {data};
+end
+
+% determine how many voxels to analyze 
+
+if ~isempty(p.Results.maskFileName)    % Get the indices from mask if specified
+    rawMask = niftiread(p.Results.maskFileName);
+    mask = rawMask(:);
+    vxs = find(mask)';
+else                                   % Analyze all voxels if no mask is specified 
+    is3d = size(data{1},4) > 1;
+    if is3d
+      dimdata = 3;
+      dimtime = 4;
+      xyzsize = sizefull(data{1},3);
+    else
+      dimdata = 1;
+      dimtime = 2;
+      xyzsize = size(data{1},1);
+    end
+    numvxs = prod(xyzsize);
+    vxs = 1:numvxs;
+end
+
+% MCR only accepts strings. This part converts variables which are passed
+% as strings to vectors and numericals.
+
+tr = str2double(tr);
+listofnums = ['0','1','2','3','4','5','6','7','8','9'];
+new_seedmode = [];
+if ~isempty(p.Results.maxpolydeg)
+    new_maxpolydeg = str2double(p.Results.maxpolyreg);
+else 
+    new_maxpolydeg = p.Results.maxpolydeg;
+end
+
+if ~isempty(p.Results.numperjob)
+    new_numperjob = str2double(p.Results.numperjob);
+else
+    new_numperjob = p.Results.numperjob;
+end
+
+if ~isempty(p.Results.hrf)
+    new_hrf = load(p.Results.hrf,'hrf');
+    new_hrf = new_hrf.hrf;
+else
+    new_hrf = p.Results.hrf;
+end 
+
+for ii = p.Results.seedmode
+    if ismember(ii, listofnums)
+        new_seedmode = [new_seedmode,str2double(ii)];
+    end
+end
+
+% Prepare the final structure and convert the remaining variables to
+% numerical
+analysisStructure = struct('vxs',vxs,'wantglmdenoise',str2double(p.Results.wantglmdenoise),'hrf',new_hrf, ...
+    'maxpolydeg',new_maxpolydeg,'seedmode',new_seedmode,'xvalmode',str2double(p.Results.xvalmode), ...
+    'numperjob',new_numperjob,'maxiter',str2double(p.Results.maxiter),'display',p.Results.display, ...
+    'typicalgain',str2double(p.Results.typicalgain));
+
+% Run the function and save the results
+results = analyzePRF(stimulus,data,tr,analysisStructure);
+save(outpath,'results')
+
+end
