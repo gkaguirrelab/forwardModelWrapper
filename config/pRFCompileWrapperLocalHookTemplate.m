91aa82dcff9514bbeb7dec8b2470255f52e97072
gkaguirre <gkaguirre@me.com>
2019-09-26 10:12:34 -0400

    Now works
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- config/pRFCompileWrapperLocalHookTemplate.m
+++ config/pRFCompileWrapperLocalHookTemplate.m
@@ -26,29 +26,34 @@ end
 
 %% Specify and save project location
 projectBaseDir = tbLocateProject(projectName);
-setpref(toolboxName,'projectBaseDir',projectBaseDir);
+setpref(projectName,'projectBaseDir',projectBaseDir);
 
 
 %% Set flywheel API key as a preference
 flywheelAPIKey='Copy this value from flywheel and paste here';
-setpref(toolboxName,'flywheelAPIKey',flywheelAPIKey);
+setpref(projectName,'flywheelAPIKey',flywheelAPIKey);
 
 
 %% Set the workbench command path
 if ismac
-setpref(toolboxName,'wbCommand','/Applications/workbench/bin_macosx64/wb_command');
+setpref(projectName,'wbCommand','/Applications/workbench/bin_macosx64/wb_command');
 end
+
+
+%% Get the userID
+[~, userID] = system('whoami');
+userID = strtrim(userID);
 
 
 %% Paths to store flywheel data and scratch space
 if ismac
     % Code to run on Mac plaform
-    setpref(toolboxName,'flywheelScratchDir','/tmp/flywheel');
-    setpref(toolboxName,'flywheelRootDir',fullfile('/Users/',userID,'/Documents/flywheel'));
+    setpref(projectName,'flywheelScratchDir','/tmp/flywheel');
+    setpref(projectName,'flywheelRootDir',fullfile('/Users/',userID,'/Documents/flywheel'));
 elseif isunix
     % Code to run on Linux plaform
-    setpref(toolboxName,'flywheelScratchDir','/tmp/flywheel');
-    setpref(toolboxName,'flywheelRootDir',fullfile('/home/',userID,'/Documents/flywheel'));
+    setpref(projectName,'flywheelScratchDir','/tmp/flywheel');
+    setpref(projectName,'flywheelRootDir',fullfile('/home/',userID,'/Documents/flywheel'));
 elseif ispc
     % Code to run on Windows platform
     warning('No supported for PC')
