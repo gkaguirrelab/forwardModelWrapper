831af16c39b94b4657d1a3042f2c0f98e68c796c
gkaguirre <gkaguirre@me.com>
2019-10-08 21:52:36 -0400

    Works
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/saveSurfMap.m
+++ code/saveSurfMap.m
@@ -1,31 +1,65 @@
-function saveSurfMap(dataPath,surfPath, varargin)
-%   Visualize a surface overlay in matlab
+function fig = saveSurfMap(dataPath,surfPath, varargin)
+% Render and save retinotopic maps on the cortical surface
 %
-%   Usage:
-%   surface_plot(p.Results.mapType,data,subject_name,hemi,surface,thresh,p.Results.alphaVal,polar_map,p.Results.viewAngle,p.Results.lightAngle)
+% Syntax:
+%  saveSurfMap(dataPath,surfPath)
 %
-%   p.Results.mapTypes:
-%       'ecc' 'pol' 'sig' 'co' 'var' 'areas' 'zstat'
-%   data = full path to data to overlay on surface or data array
-%       (note, must be in the same space as 'surface')
-%   subject_name = subject surface to plot (e.g. 'fsaverage')
-%   hemi = hemisphere ('lh', 'rh');
-%   surface = 'inflated' % 'pial' or any surface in freesurfer
-%   p.Results.alphaVal = p.Results.alphaValparancy value (0.65 - default)
-%   thresh = logical vector of the same length as data file, 0s for
-%   vertices not to show (e.g. less than some threshold).
-%   polar_map = portion of visual field. 'hemi' or 'full' ('hemi' - default);
-%   p.Results.viewAngle = view angle (default: [45,-10] for 'lh' and [-45,-10] for 'rh');
-%   p.Results.lightAngle = light angle (default: [45,-10] for 'lh' and [-45,-10] for 'rh');
+% Description
 %
-%   example: surface_plot('ecc','/home/andrew/SUBJECTS/RESTING/data/stim_lh_occipital_resting_ecc_Templ_rf.tf_avgsurf.nii.gz','fsaverage','lh',0.75)
 %
-%   written by Andrew Bock 2013
+% Inputs:
+%   dataPath              - Char vector. Full path (including file name) to
+%                           a file in MGZ format that contains a
+%                           retinotopic mapping result (e.g., eccentricity,
+%                           polar angle) in native surface space.
+%   surfpath              - Char vector. Path to the directory that
+%                           contains various surface maps for the native
+%                           space of the subject.
 %
+% Optional key/value pairs:
+%  'mapType'              - Char vector. The type of map to be displayed;
+%                           this controls the color gradient. Valid options
+%                           include:
+%                             {'ecc','pol','rsquared','sigma','areas'}
+%  'hemisphere'           - Char vector. The hemisphere to display. Valid
+%                           options are: {'lh','rh'}
+%  'whichSurface'         - Char vector. The cortical surface on which to
+%                           display the map. Valid options are:
+%                           {'inflated','pial','sphere','white'}
+%  'maxEccentricity'      - Scalar. The maximum eccentricity value
+%                           displayed (only used when mapType is ecc)
+%  'rsquaredDataPath'     - Char vector. The full path to the map of R^2
+%                           values. If set, then the rsquaredThresh value
+%                           is used to only display map values that are at
+%                           locations with R^2 values above the threshold.
+%  'rsquaredThresh'       - Scalar. The R^2 threshold that determines if a
+%                           the map value for a vertex is displayed.
+%  'alphaVal'             - Scalar. The transparency of the map color.
+%  'showCurvature'        - Logical. Whether to show the cortical surface
+%                           curvature on the render.
+%  'colorRes'             - Scalar. The number of divisions of the color
+%                           scale.
+%
+% Examples:
 %{
     dataPath = '/tmp/flywheel/v0/output/maps_nativeMGZ/R_original_eccentricity_map.mgz'
     surfPath = '/tmp/flywheel/v0/input/structZip/TOME_3021/T1w/TOME_3021/surf'
     saveSurfMap(dataPath,surfPath,'hemisphere','rh','maxEccentricity',10)
+%}
+%{
+    dataPath = '/tmp/flywheel/v0/output/maps_nativeMGZ/R_original_angle_map.mgz'
+    surfPath = '/tmp/flywheel/v0/input/structZip/TOME_3021/T1w/TOME_3021/surf'
+    saveSurfMap(dataPath,surfPath,'hemisphere','rh','mapType','pol')
+%}
+%{
+    dataPath = '/tmp/flywheel/v0/output/maps_nativeMGZ/R_original_R2_map.mgz'
+    surfPath = '/tmp/flywheel/v0/input/structZip/TOME_3021/T1w/TOME_3021/surf'
+    saveSurfMap(dataPath,surfPath,'hemisphere','rh','mapType','rsquared')
+%}
+%{
+    dataPath = '/tmp/flywheel/v0/output/maps_nativeMGZ/R_original_rfsize_map.mgz'
+    surfPath = '/tmp/flywheel/v0/input/structZip/TOME_3021/T1w/TOME_3021/surf'
+    saveSurfMap(dataPath,surfPath,'hemisphere','rh','mapType','sigma')
 %}
 
 
@@ -41,50 +75,16 @@ p.addRequired('surfPath', @ischar);
 p.addParameter('mapType', 'ecc', @ischar);
 p.addParameter('hemisphere','lh',@ischar);
 p.addParameter('whichSurface','inflated',@ischar); % pial, white, or sphere
-p.addParameter('maxEccentricity',30,@isnumeric);
-p.addParameter('alphaVal',0.85,@isnumeric);
-p.addParameter('viewAngle',[-30,-10],@isnumeric);
-p.addParameter('lightAngle',[-30,-10],@isnumeric);
+p.addParameter('maxEccentricity',30,@isscalar);
+p.addParameter('rsquaredDataPath','',@ischar);
+p.addParameter('rsquaredThresh',0.1,@isscalar);
+p.addParameter('alphaVal',0.85,@isscalar);
 p.addParameter('showCurvature',true,@islogical);
+p.addParameter('colorRes',200,@isscalar);
+p.addParameter('visible',true,@islogical);
 
 % parse
 p.parse(dataPath, surfPath, varargin{:})
-
-
-
-%% Define color map
-switch p.Results.mapType
-    case 'ecc'
-        mapres=[0 p.Results.maxEccentricity 200];
-        mycolormap = make_ecc_colormap(mapres);
-    case 'pol'
-        mapres=[0 360 200];
-        mycolormap = make_polar_colormap(mapres);
-    case 'sig'
-        mycolormap = flipud(jet(200));
-        mapres=[0 10 200];
-    case 'co'
-        %         mycolormap = jet(200);
-        mapres=[-1 1 200];
-        mycolormap = make_rbco_colormap(mapres);
-    case 'areas'
-        mycolormap = [
-            .75 .75 .75
-            1 1 1
-            .75 .75 .75
-            1 1 1
-            .75 .75 .75
-            1 1 1
-            .75 .75 .75
-            1 1 1
-            .75 .75 .75
-            1 1 1
-            ];
-        mapres=[-5 5 length(mycolormap)];
-    otherwise
-        error('Unrecognized mapType');
-end
-myvec = linspace(mapres(1),mapres(2),size(mycolormap,1));
 
 
 %% Load surface files
@@ -127,8 +127,43 @@ srf = load_mgh(dataPath);
 
 
 %% Threshold data
-% Want to implement thresholding based on R^2 map
-%srf(srf<thresh) = nan;
+if ~isempty(p.Results.rsquaredDataPath)
+    rsquared_srf = load_mgh(p.Results.rsquaredDataPath);
+    srf(rsquared_srf<p.Results.rsquaredThresh) = nan;
+end
+
+
+%% Define color map
+switch p.Results.mapType
+    case 'ecc'
+        mapres=[0 p.Results.maxEccentricity p.Results.colorRes];
+        mycolormap = make_ecc_colormap(mapres);
+        typeLabel = 'Eccentricity [deg]';
+    case 'pol'
+        mapres=[0 360 p.Results.colorRes];
+        mycolormap = make_polar_colormap(mapres);
+        typeLabel = 'Polar angle [deg]';
+    case 'sigma'
+        mapres=[0 10 p.Results.colorRes];
+        mycolormap = flipud(jet(p.Results.colorRes));
+        typeLabel = 'Sigma [deg]';
+    case 'rsquared'
+        mapres=[0 1 p.Results.colorRes];
+        mycolormap = make_grayToRed_colormap(mapres);
+        typeLabel = 'R^2';
+    case 'hrfshift'
+        mapres=[-4 4 p.Results.colorRes];
+        mycolormap = make_blueToRed_colormap(mapres);
+        typeLabel = 'shift hrf peak time [secs]';
+    case 'areas'
+        % Need to test and develop this with the output of Noah's routine
+        %        getDistinguishableColors(n_colors,bg
+        %        mapres=[-5 5 length(mycolormap)];
+        typeLabel = 'Visual area index';
+    otherwise
+        error('Unrecognized mapType');
+end
+myvec = linspace(mapres(1),mapres(2),size(mycolormap,1));
 
 
 %% Color vertices
@@ -146,79 +181,105 @@ for i = 1:length(srf)
 end
 
 
-%% Set p.Results.alphaValparency
+%% Set the alpha transparency
 alpha_vals(~isnan(srf)) = p.Results.alphaVal;
 
 
-
 %% Make figure
-figure('units','normalized','position',[0 0 1 1]);
-% make legend first, so that brain surface is active last
-subplot(4,4,3);
-if strcmp(p.Results.mapType,'ecc') || strcmp(p.Results.mapType,'pol')
-    x = linspace(-1,+1,mapres(3)); [xx,yy] = meshgrid(x);
-    [th,r] = cart2pol(xx,yy);
-    if strcmp(p.Results.mapType,'ecc')
-        img = r;
-        img(r>1) = nan;
-    elseif strcmp(p.Results.mapType,'pol')
-        img = th;
-            img(r>1) = 360;
-    else
-        img = yy - min(yy(:));
-    end
-    img = img/nanmax(img(:))*mapres(2);
-    img(isnan(img)) = mapres(2);
-    imagesc(x,x,img)
-end
-if ~strcmp(p.Results.mapType,'pol') && ~strcmp(p.Results.mapType,'ROI') && ~strcmp(p.Results.mapType,'image')
-    cb = colorbar;
-    caxis([mapres(1) mapres(2)]);
-end
-axis tight equal off
-% if strcmp(p.Results.mapType,'pol')
-%     legendmap = [.8 .8 .8;.8 .8 .8;mycolormap];
-% else
-if strcmp(p.Results.mapType,'areas') || strcmp(p.Results.mapType,'blueareas') ...
-        || strcmp(p.Results.mapType,'co') || strcmp(p.Results.mapType,'rbco')
-    legendmap = mycolormap;
+if p.Results.visible
+    fig = figure('units','normalized','position',[0 0 .35 .25]);
 else
-    if strcmp(p.Results.mapType,'pol')
-        legendmap = mycolormap;
-    else
-        legendmap = [mycolormap;.8 .8 .8];
-    end
+    fig = figure('units','normalized','position',[0 0 .35 .25],'visible','off');
+    
 end
-% end
-if strcmp(p.Results.mapType,'image')
-else
-    colormap(legendmap)
-end
-% Flip map if polar angle (e.g. upper visal field - ventral surface)
-% if strcmp(p.Results.mapType,'pol')
-%     set(cb,'YDir','reverse')
-% else
-%     set(cb,'YDir','normal')
-% end
-
 
 %% Plot brain and surface map
 smp = brain;
 smp.facevertexcdata = cmap_vals;
 %set(gcf,'name',data);
-subplot(1,2,1); hold on
-hbrain = patch(brain,'EdgeColor','none','facecolor','interp','FaceAlpha',1);
-hmap = patch(smp,'EdgeColor','none','facecolor','interp','FaceAlpha','flat'...
-    ,'FaceVertexAlphaData',alpha_vals,'AlphaDataMapping','none');
-daspect([1 1 1]);
-% Camera settings
-cameratoolbar;
-camproj perspective; % orthographic; perspective
-lighting phong; % flat; gouraud; phong
-material dull; % shiny; metal; dull
-view(p.Results.viewAngle(1),p.Results.viewAngle(2));
-%lightangle(p.Results.lightAngle(1),p.Results.lightAngle(2));
-hcamlight = camlight('headlight');
-axis tight off;
-%zoom(2)
-% note - to delete light, type 'delete(findall(gcf,'Type','light'))'
+
+% Get the plot limits
+rl_pa_is_Max = ceil(nanmax(brain.vertices)/10)*10;
+rl_pa_is_Min = floor(nanmin(brain.vertices)/10)*10;
+
+% Subplot range
+spr = [1 7];
+
+% Set the properties of each of the four view subplots
+subSet = {[1 2 3 4 5]};
+axisOrder = {[1 2]};
+
+switch p.Results.hemisphere
+    case 'lh'
+        viewSet = {[45 0]};
+    case 'rh'
+        viewSet = {[-45 0]};
+end
+
+for vv = 1:length(subSet)
+    % Set the frame for the plot
+    subplot(spr(1),spr(2),subSet{vv});
+    
+    % Brain surface
+    patch(brain,'EdgeColor','none','facecolor','interp','FaceAlpha',1);
+    hold on
+    
+    % Color map
+    patch(smp,'EdgeColor','none','facecolor','interp','FaceAlpha','flat',...
+        'FaceVertexAlphaData',alpha_vals,'AlphaDataMapping','none');
+    
+    % Set the view
+    view(viewSet{vv});
+    
+    % Hide the axis and make it tight
+    axis tight equal off;
+    
+    % Set the aspect ratio and limits
+    daspect([1 1 1]);
+    ao = axisOrder{vv};
+    xlim([rl_pa_is_Min(ao(1)) rl_pa_is_Max(ao(1))]);
+    ylim([rl_pa_is_Min(ao(2)) rl_pa_is_Max(ao(2))]);
+    
+    % Camera settings
+    camproj perspective; % orthographic; perspective
+    lighting phong; % flat; gouraud; phong
+    material dull; % shiny; metal; dull
+    camlight('headlight');
+    
+end
+
+
+%% Add the legend
+subplot(spr(1),spr(2),[6 7]);
+
+switch p.Results.mapType
+    case 'ecc'
+        x = linspace(-1,+1,mapres(3)); [xx,yy] = meshgrid(x);
+        [~,r] = cart2pol(xx,yy);
+        img = r;
+        img(r>1) = nan;
+        img = img/nanmax(img(:))*mapres(2);
+        img(isnan(img)) = mapres(2);
+        imagesc(x,x,img)
+        colormap([mycolormap; 1 1 1])
+    case 'pol'
+        x = linspace(-1,1,mapres(3));
+        [xx,yy] = meshgrid(x);
+        [img,r] = cart2pol(xx,yy);
+        img = ((img./pi)+1)*mapres(2)/2;
+        img(r>1) = mapres(2);
+        imagesc(x,x,img)
+        caxis([mapres(1) mapres(2)]);
+        colormap([mycolormap; 1 1 1])
+    otherwise
+        caxis([mapres(1) mapres(2)]);
+        colormap(mycolormap);
+end
+h = colorbar('southoutside');
+xlabel(h, typeLabel)
+axis tight equal off
+
+
+end % Main function
+
+
