6033e7d309de466fd99df9920040302d798e0077
Ozenc Taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2019-10-13 17:45:30 -0400

    Some changes for the Bayes gear

---
+++ fw_gears/bayesianFitting/construction_tools/compile_renderInferredMaps.py
@@ -0,0 +1,26 @@
+import os 
+import sys
+
+def compile_renderInferredMaps(path_to_matlab_documents, output_folder):
+    
+    # This function compiles the renderInferredMaps.m MATLAB function for the 
+    # BayesPRF gear.
+    
+    # Run this script from the terminal: python <path_to_this_function> <path_to_matlab_documents> <output_folder>
+
+    # Create the output folder if doesn't exist
+    if not os.path.exists(output_folder):
+        os.system("mkdir %s"%output_folder)
+    
+    #mcc_path = '/usr/local/MATLAB/R2018b/bin/mcc'
+    mcc_path = 'mcc'
+    mcc_call = '%s -m -R -nodisplay %s -I %s -I %s -d %s -v'%(mcc_path,
+                                                     os.path.join(path_to_matlab_documents,'projects/pRFCompileWrapper/code/renderInferredMaps.m'),
+                                                     os.path.join(path_to_matlab_documents,'projects/pRFCompileWrapper/code/'),
+                                                     os.path.join(path_to_matlab_documents,'projects/pRFCompileWrapper/code/utilities/'),
+                                                     output_folder)
+    
+    print('Compiling renderInferredMaps.m')
+    os.system(mcc_call)
+    
+compile_renderInferredMaps(*sys.argv[1:])
