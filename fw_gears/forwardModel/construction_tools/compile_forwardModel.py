f505663d3e4006599918da69ff61401b068e02d2
Ozenc Taskin <HuseyinOzenc.Taskin@pennmedicine.upenn.edu>
2019-11-08 13:27:36 -0500

    forwardmodel gear 0.4.9

--- fw_gears/forwardModel/construction_tools/compile_forwardModel.py
+++ fw_gears/forwardModel/construction_tools/compile_forwardModel.py
@@ -1,7 +1,7 @@
 import os 
 import sys
 
-def compile_forwardModel(path_to_matlab_documents, output_folder):
+def compile_forwardModel(path_to_matlab_documents='/home/ozzy/Documents/MATLAB/', output_folder='/home/ozzy/Documents/MATLAB/projects/forwardModelWrapper/fw_gears/forwardModel/forwardModel_frame/func_input'):
     
     # This function compiles the mainPRF.m. It works with ToolboxToolbox folder 
     # organization.Therefore, it assumes that the required MATLAB functions are 
