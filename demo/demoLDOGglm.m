09a33ba86d59a6e7a377515fd48edb205eee43f9
gkaguirre <gkaguirre@me.com>
2021-02-02 13:56:14 -0500

    Updated this demo

--- demo/demoLDOGglm.m
+++ demo/demoLDOGglm.m
@@ -9,30 +9,37 @@
 clear
 
 
-% Use the average signal in the mask region
-averageVoxels = '1';
-
 
 %% Variable declaration
 projectName = 'forwardModelWrapper';
 scratchSaveDir = getpref(projectName,'flywheelScratchDir');
-subjectName = 'N292';
+subjectName = 'EM529';
 
 % TR of the acquisition in seconds
 tr = '3.0';
 
 % HRF parameters for the ldog data
 hrfParams = '[-0.7520, 0.8389, 0.1139]';
-polyDeg = '5';
+polyDeg = '13';
 
-% Flag to average the acquisitions together before computing pRF
-% parameters. This makes the operation faster.
-averageAcquisitions = '0';
+% Flag to average the acquisitions together.
+averageAcquisitions = '1';
 
+% Use the average signal in the mask region
+averageVoxels = '0';
 
+% Pad missing TRs
+padTruncatedTRs = '1';
 
-%% Download the functional data
-outputFileSuffix = 'N292_lightFluxFlicker.zip';
+%% Download the functional data inputs
+% To find these, get the ID for the session (which is in the URL of the web
+% GUI, and then use this command to get a list of the analyses associated
+% with that session, and then find the analysis ID the we want.
+%
+%{
+    sessionID = '60146008afe25f7a6015acd6';
+    analysisList = fw.getSessionAnalyses('sessionID');
+%}
 
 % Create the functional tmp save dir if it does not exist
 saveDir = fullfile(scratchSaveDir,'v0','input','funcZip');
@@ -40,28 +47,31 @@ if ~exist(saveDir,'dir')
     mkdir(saveDir);
 end
 
+
 % Create a flywheel object
 fw = flywheel.Flywheel(getpref(projectName,'flywheelAPIKey'));
-
-%% Download the functional data
-% photoFlicker1, LplusS FIX output
-analysisID = '5e444eb96dea31209d2a742e';
-fileName = 'N292_photoFlicker_1_LplusS.zip';
-funcZipPath01 = fullfile(saveDir,fileName);
-fw.downloadOutputFromAnalysis(analysisID,fileName,funcZipPath01);
-
-% photoFlicker2, LplusS FIX output
-analysisID = '5e4592d16dea3124dd2a7443';
-fileName = 'N292_photoFlicker_2_LplusS.zip';
-funcZipPath02 = fullfile(saveDir,fileName);
-fw.downloadOutputFromAnalysis(analysisID,fileName,funcZipPath02);
 
 
 %% Download the stim file
 projectID = '5bb4ade9e849c300150d0d99';
-fileName = 'photoFlicker_LplusS_acq_01_06_08_x2_stimulus.mat';
+fileName = 'blockedStimulus_1x144_On=1.mat';
 stimFilePath = fullfile(saveDir,fileName);
-fw.downloadFileFromProject(projectID,fileName,stimFilePath)
+fw.downloadFileFromProject(projectID,fileName,stimFilePath);
+
+
+%% Download the functional data
+% photoFlicker1, LminusS FIX output
+analysisID = '6019723149214bc3ae649389';
+fileName = 'EM529_photoFlicker_1_LminusS_RightEye_trimmed.zip';
+funcZipPath01 = fullfile(saveDir,fileName);
+fw.downloadOutputFromAnalysis(analysisID,fileName,funcZipPath01);
+
+% photoFlicker1, LplusS FIX output
+analysisID = '601972345b34e1456d15ab39';
+fileName = 'EM529_photoFlicker_1_LminusS_RightEye.zip';
+funcZipPath02 = fullfile(saveDir,fileName);
+fw.downloadOutputFromAnalysis(analysisID,fileName,funcZipPath02);
+
 
 
 %% Download the structural data
@@ -72,15 +82,16 @@ if ~exist(saveDir,'dir')
     mkdir(saveDir);
 end
 
-analysisID = '5e4304dd6dea311bba2a7436';
-fileName = 'N292_preprocessedStruct.zip';
+analysisID = '601745597cad7bd53015acd6';
+fileName = 'EM529_preprocessedStruct.zip';
 structZipPath = fullfile(saveDir,fileName);
+fw.downloadOutputFromAnalysis(analysisID,fileName,structZipPath);
 
 % Download the mask file
-sessionID = '5dcafb3fe74aa3005e1a04e6';
-fileName = 'N292_R2_map_thresh_0.3.nii.gz';
+projectID = '5bb4ade9e849c300150d0d99';
+fileName = '2x2x2resampled_invivoTemplate.nii.gz';
 maskFilePath = fullfile(saveDir,fileName);
-fw.downloadFileFromSession(sessionID,fileName,maskFilePath)
+fw.downloadFileFromProject(projectID,fileName,maskFilePath)
 
 
 
@@ -105,10 +116,11 @@ modelOpts = ['{' ...
                '}'];
 
 %% Call the main routine
-mainWrapper(funcZipPath01,funcZipPath02,'Na','Na','Na','Na','Na','Na','Na','Na', stimFilePath, structZipPath, ...
+mainWrapper(funcZipPath01,funcZipPath02,'Na','Na','Na','Na','Na','Na','Na','Na','Na','Na','Na','Na','Na', stimFilePath, structZipPath, ...
     'maskFilePath',maskFilePath, ...
     'averageAcquisitions',averageAcquisitions, ...
     'averageVoxels',averageVoxels, ...
+    'padTruncatedTRs',padTruncatedTRs, ...
     'tr',tr, ...
     'modelClass','glm', ...
     'modelOpts',modelOpts, ...
