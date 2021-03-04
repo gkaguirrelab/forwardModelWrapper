5de69a13d85b5a8cfe3e7b494fadf763985867b0
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-03-04 00:16:47 -0500

    Fixed some small file name and terminal run issues

--- code/cifti_to_freesurfer.py
+++ code/cifti_to_freesurfer.py
@@ -1,6 +1,6 @@
 import os, sys
 
-def ciftiToFreesurfer(cifti_image, path_to_workbench, path_to_freesurfer_bin, path_to_subject_freesurfer, standard_mesh_atlases_folder, subject_id, workdir, native_mgz, fsaverage_mgz):
+def cifti_to_freesurfer(cifti_image, path_to_workbench, path_to_freesurfer_bin, path_to_subject_freesurfer, standard_mesh_atlases_folder, subject_id, workdir, native_mgz, fsaverage_mgz):
     
     '''
     This script maps cifti images to freesurfer native and fsaverage surfaces
@@ -17,6 +17,13 @@ def ciftiToFreesurfer(cifti_image, path_to_workbench, path_to_freesurfer_bin, pa
         fsaverage_mgz = Folder where the fsaverage mgz results will be saved
     ''' 
         
+    # Create the workdir, native and fsavrage folders if they don't exist
+    if not os.path.exists(workdir):
+        os.system('mkdir %s' % workdir)
+    if not os.path.exists(native_mgz):
+        os.system('mkdir %s' % native_mgz)
+    if not os.path.exists(fsaverage_mgz):
+        os.system('mkdir %s' % fsaverage_mgz)
     
     # Set new paths for cifti hemispheres
     cifti_left = os.path.join(workdir, 'cifti_left.func.gii')
@@ -38,7 +45,7 @@ def ciftiToFreesurfer(cifti_image, path_to_workbench, path_to_freesurfer_bin, pa
     new_sphere_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage', 'fsaverage_std_sphere.R.164k_fsavg_R.surf.gii')
     metric_out_right = os.path.join(workdir, '%s.R.32k_fsavg_R.func.gii' % subject_id)
     current_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage', 'fs_LR.R.midthickness_va_avg.32k_fs_LR.shape.gii')
-    new_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage', 'fsaverage.L.midthickness_va_avg.164k_fsavg_R.shape.gii')
+    new_area_right = os.path.join(standard_mesh_atlases_folder, 'resample_fsaverage', 'fsaverage.R.midthickness_va_avg.164k_fsavg_R.shape.gii')
     
     # Run fsaverage conversion 
     left_hemi_run = '%s -metric-resample %s %s %s ADAP_BARY_AREA %s -area-metrics %s %s' % (os.path.join(path_to_workbench, 'wb_command'),
@@ -67,5 +74,4 @@ def ciftiToFreesurfer(cifti_image, path_to_workbench, path_to_freesurfer_bin, pa
     os.system('SUBJECTS_DIR=%s; %s --srcsubject fsaverage --trgsubject %s --hemi rh --sval %s --tval %s' % (path_to_subject_freesurfer,
                                                                                                             os.path.join(path_to_freesurfer_bin, 'mri_surf2surf'),
                                                                                                             subject_id, metric_out_right_mgz, native_metric_right))    
-    
-    ciftiToFreesurfer(*sys.argv[1:])
+cifti_to_freesurfer(*sys.argv[1:])  
